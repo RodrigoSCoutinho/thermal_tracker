1129617e7b050b761bc7a1a08de86a1822cf7175
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2025-09-20 20:53:19 -0300

    PJ-03: Implement main application structure with temperature monitoring features and update README.md

---
+++ thermal_app/lib/services/api_service.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseUrl;
+  final String apiKey;
+
+  ApiService({required this.baseUrl, required this.apiKey});
+
+  Future<Map<String, dynamic>> fetchLatestData() async {
+    final response = await http
+        .get(Uri.parse('$baseUrl/feeds.json?api_key=$apiKey&results=1'));
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to fetch data');
+    }
+  }
+
+  Future<List<dynamic>> fetchHistoricalData() async {
+    final response =
+        await http.get(Uri.parse('$baseUrl/feeds.json?api_key=$apiKey'));
+    if (response.statusCode == 200) {
+      return json.decode(response.body)['feeds'];
+    } else {
+      throw Exception('Failed to fetch historical data');
+    }
+  }
+}
